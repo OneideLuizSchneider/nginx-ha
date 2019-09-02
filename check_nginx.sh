3b80f02d59ad8513a267a2c009559fe60ed90b92
Oneide Luiz Schneider <oneidels@gmail.com>
2019-09-02 16:40:00 -0300

    Create check_nginx.sh

---
+++ check_nginx.sh
@@ -0,0 +1,13 @@
+export DO_TOKEN='yout_token'
+IP='floating_ip'
+ID=$(curl -s http://169.254.169.254/metadata/v1/id)
+HAS_FLOATING_IP=$(curl -s http://169.254.169.254/metadata/v1/floating_ip/ipv4/active)
+if [ $HAS_FLOATING_IP = "false" ]; then
+    n=0
+    while [ $n -lt 10 ]
+    do
+        python /usr/local/bin/assign-ip $IP $ID && break
+        n=$((n+1))
+        sleep 3
+    done
+fi
